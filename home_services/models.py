e0bbce411c64b17a59c69dd4b35923f1fb2e5ea7
rghosh <rghosh.ds@gmail.com>
2024-11-29 20:32:57 +0530

    add log in functionality

--- home_services/models.py
+++ home_services/models.py
@@ -1,43 +1,29 @@
-from flask_sqlalchemy import SQLAlchemy
+from home_services.extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-
-db = SQLAlchemy()
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-
-class Admin(User):
-    __tablename__ = 'admins'
-
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+        return check_password_hash(self.password_hash, password)
 
 
 class Customer(User):
     __tablename__ = 'customers'
-
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     name = db.Column(db.String(120), nullable=False)
 
 
 class Professional(User):
     __tablename__ = 'professionals'
-
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     service_type = db.Column(db.String(120), nullable=False)
@@ -51,15 +37,12 @@ class Service(db.Model):
     time_required = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-    def __repr__(self):
-        return f"Service('{self.name}', '{self.price}', '{self.time_required}')"
-
 
 class ServiceRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
-    professional_id = db.Column(db.Integer, db.ForeignKey('professional.id'), nullable=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    professional_id = db.Column(db.Integer, db.ForeignKey('professionals.id'), nullable=True)
     date_of_request = db.Column(db.DateTime, default=datetime.utcnow)
     date_of_completion = db.Column(db.DateTime, nullable=True)
     service_status = db.Column(db.String(50), nullable=False)
@@ -68,10 +51,3 @@ class ServiceRequest(db.Model):
     service = db.relationship('Service', backref='requests')
     customer = db.relationship('Customer', backref='requests')
     professional = db.relationship('Professional', backref='requests', lazy=True)
-
-    def __repr__(self):
-        return (f"ServiceRequest('{self.id}',"
-                f" '{self.service_id}',"
-                f" '{self.customer_id}',"
-                f" '{self.professional_id}',"
-                f" '{self.service_status}')")
--- home_services/routes.py
+++ home_services/routes.py
@@ -1,13 +1,34 @@
-from flask import render_template, Flask, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for, flash, jsonify
+from home_services.forms import LoginForm
+from home_services.models import User
+from home_services.extensions import db, bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+import email_validator
 
 core = Blueprint('core', __name__)
 
 
-@core.route('/')
+@core.route('/', methods=["GET", "POST"])
 def home():
     return render_template("home.html", css_file="home.css")
 
 
-@core.route('/login')
+@core.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html', css_file=None)
+    form = LoginForm()
+    error_message = None
+    if form.validate_on_submit():
+        print("Form submitted successfully")
+        print(f"Email: {form.email.data}")
+        user = User.query.filter_by(email=form.email.data).first()
+        print(f"User found: {user}")
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
+            print("Password check passed")
+            access_token = create_access_token(identity=user.id)
+            return jsonify({'access_token': access_token, 'message': 'Login successful', 't': 'loginSuccess'}), 200
+        else:
+            print("Login unsuccessful. Invalid email or password")
+            error_message = 'Login Unsuccessful. Please check email and password'
+    else:
+        print("Form validation failed")
+    return render_template('login.html', form=form, css_file="login.css", error_message=error_message)
